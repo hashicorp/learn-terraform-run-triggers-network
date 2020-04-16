8823701b2fe30041691f2a073e1de94ddf87e929
Robin Norwood <rnorwood@hashicorp.com>
2020-04-16 12:32:42 -0500

    Don't give target group explicit name

--- main.tf
+++ main.tf
@@ -235,7 +235,6 @@ resource "aws_lb_listener" "http" {
 # }
 
 resource "aws_lb_target_group" "http" {
-  name     = "tf-example-lb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.example.id
