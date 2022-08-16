1121ec00b3707f8690a299bff6d8e3d99e9dbf7c
Armin <armin_mesic@hotmail.de>
2022-08-16 14:02:57 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable aws_region {
 variable project_tag {
   description = "Value of the 'Project' tag for all resources"
   type        = string
-  default     = "learn-terraform-run-triggers"
+  default     = "project-2"
 }
 
 variable vpc_cidr_block {
